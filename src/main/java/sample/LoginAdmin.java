b6364a79f1f5c6bf7d93752935d7fa046a90688f
Pang Prapawarin <prapawarin.k@ku.th>
2020-09-23 21:55:23 +0700

    add maven to project

---
+++ src/main/java/sample/LoginAdmin.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginAdmin {
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Button loginAdminButton;
+
+    @FXML public void handleLoginAdminButton(ActionEvent event) throws IOException {
+        Button b = (Button) event.getSource();
+        Stage stage = (Stage) b.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/me/login_success_admin.fxml"));
+
+        stage.setScene(new Scene(loader.load(),600,600));
+        PopUpRecommend l = loader.getController();
+        stage.show();
+    }
+}
---
+++ src/main/java/sample/LoginGuest.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginGuest {
+    @FXML
+    private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Button loginGuestButton;
+    @FXML private Label forgetPassword;
+
+    @FXML public void handleLoginStaffButton(ActionEvent event) throws IOException {
+        Button b = (Button) event.getSource();
+        Stage stage = (Stage) b.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/me/login_success_guest.fxml"));
+
+        stage.setScene(new Scene(loader.load(),600,600));
+        PopUpRecommend l = loader.getController();
+        stage.show();
+    }
+}
